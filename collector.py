f7844548869f38f707e6fb07c2d241e2ba6d89a6
Ben Williams <bcbwilla@gmail.com>
2014-07-25 12:59:42 -0400

    add graceful exit when keyboard interrupted

--- collector.py
+++ collector.py
@@ -1,5 +1,7 @@
 """ Define the class which listens to the twitter stream and run it. """
 
+import sys
+
 from tweepy import Stream, OAuthHandler
 
 import sw_auth as swa
@@ -9,7 +11,11 @@ from skatewords import StdOutListener
 auth = OAuthHandler(swa.consumer_key, swa.consumer_secret)
 auth.set_access_token(swa.access_token, swa.access_token_secret)
 
-# run stream
-l = StdOutListener()
-stream = Stream(auth, l)
-stream.filter(track=['skateboard','skateboarding','skateboarder'])
+try:
+    # run stream
+    l = StdOutListener()
+    stream = Stream(auth, l)
+    stream.filter(track=['skateboard','skateboarding','skateboarder'])
+except KeyboardInterrupt:
+    print "Stopping twitter stream collector."
+    sys.exit()
